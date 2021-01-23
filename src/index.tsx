ee35adeb3144f80746d16add3fa9b52943145a6b
wallypark710 <wallypark710@gmail.com>
2021-01-23 17:22:17 +0900

    Feature: 리덕스 기본 셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '@src/store';
 import GlobalStyles from '@src/styles/GlobalStyles';
 import App from '@src/App';
 
 ReactDOM.render(
   <React.Fragment>
     <GlobalStyles />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.Fragment>,
   document.getElementById('root')
 );
